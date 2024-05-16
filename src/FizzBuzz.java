85f3b834488cd7f68dcff5baf1437768268f18a8
Robert-0-ai <137901260+Robert-0-ai@users.noreply.github.com>
2024-05-16 18:52:53 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -7,11 +7,11 @@ class FizzBuzz {
         int i = 1;
         while (i < 100) {
 
-            i = dofizbuzz(i);
+            i = getfizzbuzz(i);
         }
     }
 
-    private static int dofizbuzz(int i) {
+    private static int getfizzbuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
